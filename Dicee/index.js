4b064791413bab9061551b4e2431ee039c98e7cd
nitin-agam <nitinagam17@gmail.com>
2023-03-10 19:03:21 +0530

    Added Dicee Project

---
+++ Dicee/index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage1;
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins !!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins !!";
+} else {
+    document.querySelector("h1").innerHTML = "Match Drawn !!";
+}
